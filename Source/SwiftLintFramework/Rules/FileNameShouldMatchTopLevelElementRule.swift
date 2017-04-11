32ea7503c22985872c5c808cb8c1d311427869f4
Andrey Kopnin <a.kopnin@gmail.com>
2017-04-11 15:11:44 +0300

    Added 'FileNameShouldMatchTopLevelElement' rule. Added handling of extensions.

--- Source/SwiftLintFramework/Rules/FileNameShouldMatchTopLevelElementRule.swift
+++ Source/SwiftLintFramework/Rules/FileNameShouldMatchTopLevelElementRule.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Realm. All rights reserved.
 //
 
+import Foundation
 import SourceKittenFramework
 
 public struct FileNameShouldMatchTopLevelElementRule: OptInRule, ConfigurationProviderRule {
@@ -47,14 +48,66 @@ public struct FileNameShouldMatchTopLevelElementRule: OptInRule, ConfigurationPr
         if let topLevelDeclaration = topLevelDeclaration {
         
             let fileUrl = URL(fileURLWithPath: filePath)
-            let fileName = fileUrl.lastPathComponent
-            if fileName != topLevelDeclaration.name {
-                results.append(StyleViolation(
-                    ruleDescription: FileNameShouldMatchTopLevelElementRule.description,
-                    location: Location(file: file, byteOffset: topLevelDeclaration.offset!)))
+            let fileName = fileUrl.deletingPathExtension().lastPathComponent
+           
+            if topLevelDeclaration.kind == SwiftDeclarationKind.extension.rawValue {
+                
+                if !self.validateExtensionName(topLevelDeclaration.name!, fileName) {
+                    results.append(StyleViolation(ruleDescription: FileNameShouldMatchTopLevelElementRule.description,
+                                                  severity: .warning,
+                                                  location: Location(file: file, byteOffset: topLevelDeclaration.offset!),
+                                                  reason: "File name should match first type name: '\(topLevelDeclaration.name!).swift'"))
+                }
+            
+            } else {
+                if fileName != topLevelDeclaration.name {
+                    results.append(StyleViolation(ruleDescription: FileNameShouldMatchTopLevelElementRule.description,
+                                                  severity: .warning,
+                                                  location: Location(file: file, byteOffset: topLevelDeclaration.offset!),
+                                                  reason: "File name should match first type name: '\(topLevelDeclaration.name!).swift'"))
+                }
             }
         }
         
         return [];
+    }
+    
+    private static func validateExtensionName(_ className: String, _ fileName: String) -> Bool {
+        
+        var isValid = false
+        
+        let fileNamePattern = "(?:\(className))\\+(\\w[\\w\\d]+)"
+        let fileNameRegex = try! NSRegularExpression(pattern: fileNamePattern, options: [])
+        
+        let nsFileName = fileName as NSString
+        let range = NSRange(location: 0, length: nsFileName.length)
+        
+        let matches = fileNameRegex.matches(in: fileName, options: [], range: range)
+        if !matches.isEmpty {
+            
+            // Extension description must be a valid identifer name
+            let extensionsDescription = nsFileName.substring(with: matches[0].rangeAt(1))
+            isValid = CharacterSet.alphanumerics.isSuperset(ofCharactersIn: extensionsDescription) && !extensionsDescription.isViolatingCase
+        }
+        
+        return isValid
+    }
+}
+
+
+fileprivate extension String {
+    var isViolatingCase: Bool {
+        let secondIndex = characters.index(after: startIndex)
+        let firstCharacter = substring(to: secondIndex)
+        
+        guard firstCharacter.isUppercase() else {
+            return true
+        }
+        guard characters.count > 1 else {
+            return true
+        }
+        let range = secondIndex..<characters.index(after: secondIndex)
+        let secondCharacter = substring(with: range)
+        return secondCharacter.isLowercase()
     }
 }
